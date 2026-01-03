86ebb49c1120d48b45b7dfc8220fc6f712106750
AhmadKevin000 <ahmadkevinmalik01>
2026-01-03 22:46:05 +0700

    ui blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -9,10 +9,15 @@
             <span class="text-sm text-gray-500">Oleh Kevin Smith</span>
             <span class="text-sm text-gray-500">Diposting pada 1 Januari 2024</span>
         </div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro incidunt a fuga, neque molestiae ab fugiat! Eligendi obcaecati temporibus molestiae quae unde voluptates corrupti commodi error culpa eveniet, perferendis magnam?</p>
+        <p class="my-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro incidunt a fuga, neque molestiae ab fugiat! Eligendi obcaecati temporibus molestiae quae unde voluptates corrupti commodi error culpa eveniet, perferendis magnam?</p>
     </article>
+    
     <article class="py-8">
-        <h2>Judul Artikel 2</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro incidunt a fuga, neque molestiae ab fugiat! Eligendi obcaecati temporibus molestiae quae unde voluptates corrupti commodi error culpa eveniet, perferendis magnam?</p>
+        <h2 class="mb-2 text-2xl tracking-tight font-bold text-gray-900">Judul Artikel 2</h2>
+        <div>
+            <span class="text-sm text-gray-500">Oleh John Doe</span>
+            <span class="text-sm text-gray-500">Diposting pada 2 Januari 2024</span>
+        </div>
+        <p class="my-2 text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro incidunt a fuga, neque molestiae ab fugiat! Eligendi obcaecati temporibus molestiae quae unde voluptates corrupti commodi error culpa eveniet, perferendis magnam?</p>
     </article>  
 </x-layout>
